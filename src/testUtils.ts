b547771661e61072be44a3710d90d1de6bb554ff
James Brill <contact@jamesbrill.co.uk>
2021-05-01 14:14:10 +0100

    Remove dead code from testUtils

--- src/testUtils.ts
+++ src/testUtils.ts
@@ -2,34 +2,6 @@ import TEST_DATA from './testData';
 
 const { SENTENCE_ONE } = TEST_DATA;
 
-let _callback: any;
-export const mockOnSegmentChange = jest.fn((callback: any) => {
-  _callback = callback;
-});
-export const mockInitialize = jest.fn(() => Promise.resolve());
-export const mockStartContext = jest.fn(() => Promise.resolve());
-export const mockStopContext = jest.fn(() => Promise.resolve());
-jest.mock('@speechly/browser-client', () => ({
-  Client: function () {
-    return {
-      onSegmentChange: mockOnSegmentChange,
-      initialize: mockInitialize,
-      startContext: mockStartContext,
-      stopContext: mockStopContext,
-    };
-  }
-}));
-
-export const speak = (sentence: any) => {
-  sentence.forEach(_callback)
-}
-
-export const speakAndInterrupt = (sentence: any, interrupt: Function) => {
-  _callback(sentence[0]);
-  interrupt();
-  sentence.slice(1).forEach(_callback);
-}
-
 export const expectSentenceToBeTranscribedWithFirstInitialResult = (sentence: any, mockOnResult: any) => {
   expect(mockOnResult).toHaveBeenNthCalledWith(1, { results: [
     {
